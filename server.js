1c1c23cd12aeb64410e1a326d216b7509c4ca692
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2020-07-05 20:52:55 +0600

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/Schedule-SPA'));
+app.use(express.static(__dirname + '/dist/schedule-spa'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/Schedule-SPA/index.html'));});
+    '/dist/schedule-spa/index.html'));});
 app.listen(process.env.PORT || 8080);
